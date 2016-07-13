d79968e8d8aa009a82487960f2188e44694f7045
flashback2k14 <feedbackcalcest@gmail.com>
2016-07-13 13:04:34 +0200

    begin style list-item, add bash script to start backend

---
+++ run-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Go to JAR Path
+echo "Go To JAR File"
+
+if [ $# -eq 0 ]
+  then
+    cd /Users/flbk/Documents/Development/Java/Vertx/is-there-a-movie/build/libs
+else
+    cd $1
+fi
+
+# Run Server
+echo "Start Server"
+
+java -jar botomo-1.0-SNAPSHOT-fat.jar -Dhttp.port=8080
